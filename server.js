8adcd9950c1362c5ee037ad8e55dd39c56f563f6
Asen <asen.trichkov1@gmail.com>
2023-10-27 00:56:20 +0300

    change table struct,  fix eslint rule

--- server.js
+++ server.js
@@ -17,8 +17,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', async (req, res) => {
     const results = await new Promise((resolve, reject) => {
-        const list = [];
-        db.all('SELECT title, priority, tags FROM todos', (err, rows) => {
+        db.all('SELECT id, title, priority, tags FROM todos', (err, rows) => {
             if (err) reject(err);
             resolve(rows);
         });
@@ -38,19 +37,19 @@ function sendNotification(body, headers, topic = 'todo') {
 app.post('/create', (req, res) => {
     const { title, priority, emoji } = req.body;
 
-    sendNotification('created', {
-        Title: title,
-        Priority: priority,
-        Tags: emoji,
-    });
-
     db.serialize(() => {
-        db.run('CREATE TABLE IF NOT EXISTS todos (title TEXT, priority INTEGER, tags TEXT)');
-        const stmt = db.prepare('INSERT INTO todos VALUES (?, ?, ?)');
+        db.run('CREATE TABLE IF NOT EXISTS todos (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, priority TEXT, tags TEXT)');
+        const stmt = db.prepare('INSERT INTO todos (title, priority, tags) VALUES (?, ?, ?)');
 
         stmt.run(title, priority, emoji);
 
         stmt.finalize();
+    });
+
+    sendNotification('created', {
+        Title: title,
+        Priority: priority,
+        Tags: emoji,
     });
 
     res.json({ message: 'Task created successfully' });
