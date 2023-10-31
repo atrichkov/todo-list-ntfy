ac1a8833c103c2d57165affc8354b8ca63d0b81d
Asen <asen.trichkov1@gmail.com>
2023-10-31 23:21:16 +0200

    refactoring, add Dockerfile

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', async (req, res) => {
     const results = await new Promise((resolve, reject) => {
